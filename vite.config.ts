e07b460620c2de93bdc9caf51cbea420456527e0
Jaan Mustafa <rizabul.md@clear.in>
2026-02-11 17:49:11 +0530

    changes added

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/mdrizabul/', // Repository name for GitHub Pages
+  base: '/', // Root path for custom domain www.mdrizabul.com
 })
