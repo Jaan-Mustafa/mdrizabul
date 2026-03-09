772695d336f9b7c7ea902688c79d00b33075585c
Jaan Mustafa <mdrizabul_se22a12_23@dtu.ac.in>
2026-03-09 12:47:55 +0530

    a

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -21,7 +21,7 @@ const Header = () => {
     { id: 'experience', label: 'Experience', path: '/experience' },
     { id: 'projects', label: 'Projects', path: '/projects' },
     { id: 'blog', label: 'Blogger', path: '/blog' },
-    { id: 'contact', label: 'Contacts-for', path: '/contact' }
+    { id: 'contact', label: 'Contacts-f', path: '/contact' }
   ];
 
   const handleNavClick = () => {
